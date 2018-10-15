5c08a42111fd56d27baeddf6b1d3a2944d2a1c13
Leonardo Mopaca <leomopaca@gmail.com>
2018-10-15 19:54:32 -0300

    Organização de Repositório
    Organizando Repositório

--- AlgoritmosEstruturaDados/src/com/teste/lista/Teste.java
+++
@@ -1,9 +0,0 @@
-package com.teste.lista;
-
-public class Teste {
-    public static void main(String [] args){
-        ListaLigada lista = new ListaLigada();
-        //lista.valor = 1;
-        
-    }
-}
